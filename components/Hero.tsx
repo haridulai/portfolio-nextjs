dd3181f03716a89fafc48e69522f35fcb383436a
hdcodesdev <91739271+hdcodesdev@users.noreply.github.com>
2023-03-27 10:58:59 +0100

    hero image styled

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -12,12 +12,19 @@ function Hero({}: Props) {
   });
 
   return (
-    <div className="h-screen flex flex-col space-y-8 items-center justify-center">
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
       <BackgroundCircles />
-      <h1>
-        <span>{text}</span>
-        <Cursor cursorColor="blue" />
-      </h1>
+      <img
+        className="relative rounded-full h-32 w-32 mx-auto object-cover"
+        src="https://media.licdn.com/dms/image/D4E03AQEDSZwawOXoPg/profile-displayphoto-shrink_800_800/0/1665436043005?e=1685577600&v=beta&t=yUg_yv__l1GLZ1oxBva2cT_PS1d-nNocD4Sd9HBbeJw"
+        alt="Harinder Dulai"
+      />
+      <div>
+        <h1>
+          <span>{text}</span>
+          <Cursor cursorColor="blue" />
+        </h1>
+      </div>
     </div>
   );
 }
