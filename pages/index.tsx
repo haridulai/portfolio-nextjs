200d6774e87c306559e4271a7805c911004d2076
hdcodesdev <91739271+hdcodesdev@users.noreply.github.com>
2023-03-27 15:28:23 +0100

    ContactMe component

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import About from "@/components/About";
 import WorkExperience from "@/components/WorkExperience";
 import Skills from "@/components/Skills";
 import Projects from "@/components/Projects";
+import ContactMe from "@/components/ContactMe";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -34,11 +35,14 @@ export default function Home() {
       <section id="skills" className="snap-start">
         <Skills />
       </section>
-      {/* Projects */}
+
       <section id="projects" className="snap-start">
         <Projects />
       </section>
-      {/* Contact me */}
+
+      <section id="contact" className="snap-start">
+        <ContactMe />
+      </section>
     </div>
   );
 }
