0c23bcce9d0ca05ff713f855f76d75f8b009f414
hdcodesdev <91739271+hdcodesdev@users.noreply.github.com>
2023-03-27 11:28:10 +0100

    Hero section complete

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 import { useTypewriter, Cursor } from "react-simple-typewriter";
 import BackgroundCircles from "./BackgroundCircles";
@@ -6,7 +7,11 @@ type Props = {};
 
 function Hero({}: Props) {
   const [text, count] = useTypewriter({
-    words: ["Developer", "Designer", "Creator"],
+    words: [
+      "Hi, The Name's Harinder Dulai",
+      "Guy-who-loves-Coffee.tsx",
+      "<ButLovesToCodeMore />",
+    ],
     loop: true,
     delaySpeed: 2000,
   });
@@ -19,11 +24,28 @@ function Hero({}: Props) {
         src="https://media.licdn.com/dms/image/D4E03AQEDSZwawOXoPg/profile-displayphoto-shrink_800_800/0/1665436043005?e=1685577600&v=beta&t=yUg_yv__l1GLZ1oxBva2cT_PS1d-nNocD4Sd9HBbeJw"
         alt="Harinder Dulai"
       />
-      <div>
-        <h1>
-          <span>{text}</span>
+      <div className="z-20">
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
+          Software Engineer
+        </h2>
+        <h1 className="text-5xl lg:text-6xl font-semibold">
+          <span className="mr-3">{text}</span>
           <Cursor cursorColor="blue" />
         </h1>
+        <div className="pt-5">
+          <Link href="#about">
+            <button className="heroButton">About</button>
+          </Link>
+          <Link href="#experience">
+            <button className="heroButton">Experience</button>
+          </Link>
+          <Link href="#skills">
+            <button className="heroButton">Skills</button>
+          </Link>
+          <Link href="#projects">
+            <button className="heroButton">Projects</button>
+          </Link>
+        </div>
       </div>
     </div>
   );
