1e779f9f8a6b483c1d0b2c1fedd6fe2a1ea70203
hdcodesdev <91739271+hdcodesdev@users.noreply.github.com>
2023-03-28 22:50:02 +0100

    fetch api endpoints

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,47 +1,58 @@
-import Head from "next/head";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
-import Header from "@/components/Header";
-import Hero from "@/components/Hero";
-import About from "@/components/About";
-import WorkExperience from "@/components/WorkExperience";
-import Skills from "@/components/Skills";
-import Projects from "@/components/Projects";
-import ContactMe from "@/components/ContactMe";
+import { GetStaticProps } from "next";
+import About from "../components/About";
+import ContactMe from "../components/ContactMe";
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import Projects from "../components/Projects";
+import Skills from "../components/Skills";
+import WorkExperience from "../components/WorkExperience";
+import { Experience, PageInfo, Project, Skill, Social } from "../typings";
+import { fetchPageInfo } from "../utils/fetchPageInfo";
+import { fetchExperiences } from "../utils/fetchExperiences";
+import { fetchSkills } from "../utils/fetchSkills";
+import { fetchProjects } from "../utils/fetchProjects";
+import { fetchSocial } from "../utils/fetchSocials";
 import Link from "next/link";
-import { ArrowSmallUpIcon } from "@heroicons/react/24/solid";
 
-const inter = Inter({ subsets: ["latin"] });
+type Props = {
+  pageInfo: PageInfo;
+  experiences: Experience[];
+  skills: Skill[];
+  projects: Project[];
+  socials: Social[];
+};
 
-export default function Home() {
+const Home = ({ projects, skills, pageInfo, experiences, socials }: Props) => {
   return (
-    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
-      <Head>
-        <title>Harinder Dulai's Portfolio</title>
-      </Head>
-
+    <div className="snap-y snap-mandatory h-screen overflow-y-scroll scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80 bg-[rgb(36,36,36)] text-white z-0">
       <Header />
 
-      <section id="hero" className="snap-center">
+      {/* Hero Banner Section */}
+      <section id="hero" className="snap-start">
         <Hero />
       </section>
 
-      <section id="about" className="snap-start">
+      {/* About Section */}
+      <section id="about" className="snap-center">
         <About />
       </section>
 
+      {/* Experience Section */}
       <section id="experience" className="snap-center">
         <WorkExperience />
       </section>
 
+      {/* Skills Section */}
       <section id="skills" className="snap-start">
         <Skills />
       </section>
 
+      {/* Projects Section */}
       <section id="projects" className="snap-start">
         <Projects />
       </section>
 
+      {/* Contact Me Section */}
       <section id="contact" className="snap-start">
         <ContactMe />
       </section>
@@ -49,10 +60,38 @@ export default function Home() {
       <Link href="#hero">
         <footer className="sticky bottom-5 w-full cursor-pointer">
           <div className="flex items-center justify-center">
-            <ArrowSmallUpIcon className="h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer" />
+            <img
+              className="h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer"
+              src="https://i.imgur.com/e2yvD6A.png"
+              alt=""
+            />
           </div>
         </footer>
       </Link>
     </div>
   );
-}
+};
+
+export default Home;
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const pageInfo: PageInfo = await fetchPageInfo();
+  const experiences: Experience[] = await fetchExperiences();
+  const skills: Skill[] = await fetchSkills();
+  const projects: Project[] = await fetchProjects();
+  const socials: Social[] = await fetchSocial();
+
+  return {
+    props: {
+      pageInfo,
+      experiences,
+      skills,
+      projects,
+      socials,
+    },
+    // Next.js will attempt to re-generate the page:
+    // - When a request comes in
+    // - At most once every 10 seconds
+    revalidate: 10,
+  };
+};
