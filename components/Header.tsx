5f99bb79ab3bde714bd721b4522f2915416d6e41
hdcodesdev <91739271+hdcodesdev@users.noreply.github.com>
2023-03-29 14:09:01 +0100

    map social prop to header social icon

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import { SocialIcon } from "react-social-icons";
 import { motion } from "framer-motion";
 import Link from "next/link";
-type Props = {};
+import { Social } from "@/typings";
 
-function Header({}: Props) {
+type Props = {
+  socials: Social[];
+};
+
+function Header({ socials }: Props) {
   return (
     <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
       <motion.div
@@ -13,27 +17,28 @@ function Header({}: Props) {
         transition={{ duration: 1.5 }}
         className="flex flex-row items-center"
       >
-        <SocialIcon
-          url="https://twitter.com/jaketrent"
-          fgColor="gray"
-          bgColor="transparent"
-        />
-        <SocialIcon
-          url="https://twitter.com/jaketrent"
-          fgColor="gray"
-          bgColor="transparent"
-        />
-        <SocialIcon
-          url="https://twitter.com/jaketrent"
-          fgColor="gray"
-          bgColor="transparent"
-        />
+        {socials.map((social) => (
+          <SocialIcon
+            key={social._id}
+            url={social.url}
+            fgColor="gray"
+            bgColor="transparent"
+          />
+        ))}
       </motion.div>
       <Link href="#contact">
         <motion.div
-          initial={{ x: 500, opacity: 0, scale: 0.5 }}
-          animate={{ x: 0, opacity: 1, scale: 1 }}
-          transition={{ duration: 1.5 }}
+          initial={{
+            x: 500,
+            opacity: 0,
+            scale: 0.5,
+          }}
+          animate={{
+            x: 0,
+            opacity: 1,
+            scale: 1,
+          }}
+          transition={{ duration: 1 }}
           className="flex flex-row items-center text-gray-300 cursor-pointer"
         >
           <SocialIcon
@@ -43,7 +48,7 @@ function Header({}: Props) {
             bgColor="transparent"
           />
           <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
-            Get in touch
+            Get In Touch
           </p>
         </motion.div>
       </Link>
--- pages/index.tsx
+++ pages/index.tsx
@@ -13,6 +13,7 @@ import { fetchSkills } from "../utils/fetchSkills";
 import { fetchProjects } from "../utils/fetchProjects";
 import { fetchSocial } from "../utils/fetchSocials";
 import Link from "next/link";
+import { ArrowSmallUpIcon } from "@heroicons/react/24/solid";
 
 type Props = {
   pageInfo: PageInfo;
@@ -25,7 +26,7 @@ type Props = {
 const Home = ({ projects, skills, pageInfo, experiences, socials }: Props) => {
   return (
     <div className="snap-y snap-mandatory h-screen overflow-y-scroll scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80 bg-[rgb(36,36,36)] text-white z-0">
-      <Header />
+      <Header socials={socials} />
 
       {/* Hero Banner Section */}
       <section id="hero" className="snap-start">
@@ -60,11 +61,7 @@ const Home = ({ projects, skills, pageInfo, experiences, socials }: Props) => {
       <Link href="#hero">
         <footer className="sticky bottom-5 w-full cursor-pointer">
           <div className="flex items-center justify-center">
-            <img
-              className="h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer"
-              src="https://i.imgur.com/e2yvD6A.png"
-              alt=""
-            />
+            <ArrowSmallUpIcon className="h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer" />
           </div>
         </footer>
       </Link>
